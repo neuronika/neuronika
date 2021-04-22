e4833807d7c709d215d8998ae01de47751148b3d
frjnn <f.iannelli.francesco229@gmail.com>
2021-04-22 10:43:35 +0200

    Begin rework - Add new Inputs and Vars

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,87 +1,127 @@
 pub mod node;
 
-use itertools::Itertools;
+use itertools::{Diff, Itertools};
 use ndarray::{Array, DimMax, Dimension, Ix1, Ix2, Ix3, Ix4, Ix5, Ix6, IxDyn, RemoveAxis};
 // use node::{
 //     Addition, Concatenate, Division, Exp, LeakyRelu, Logn, MatrixMatrixMul, MatrixVectorMul,
 //     Multiplication, Negation, Node, Parameter, Power, Relu, Sigmoid, Softmax, Softplus, Stack,
 //     Subtraction, Sum, Tanh, Transpose, Unsqueeze, VectorMatrixMul, VectorVectorMul,
 // };
-use node::{Input, Node, Parameter};
+use node::{Backward, Data, Differentiable, Forward, Gradient};
 use std::{
     cell::{Ref, RefCell, RefMut},
+    collections::BTreeMap,
     fmt::Debug,
     ops::{Add, Deref, Div, Mul, Neg, Sub},
     rc::Rc,
 };
 
+use crate::Input;
+
+use self::node::InputDiff;
+
 pub(crate) type Broadcasted<Lhs, Rhs> = <Lhs as DimMax<Rhs>>::Output;
 pub(crate) type BroadTensor<Lhs, Rhs> = Tensor<Broadcasted<Lhs, Rhs>>;
 pub(crate) type Tensor<D> = Array<f32, D>;
 
+struct OperationsCounter {
+    count: usize,
+}
+
+impl OperationsCounter {
+    pub fn next(&mut self) -> usize {
+        self.count += 1;
+        self.count
+    }
+}
+
+static mut OPERATIONS_COUNTER: OperationsCounter = OperationsCounter { count: 0 };
+
 pub trait ParamDim: Dimension + 'static {
-    fn insert(item: GraphBuilder<Parameter<Self>>, dest: &mut Parameters);
+    fn insert(item: Param<Self>, dest: &mut Parameters);
 }
 
 impl ParamDim for Ix1 {
-    fn insert(item: GraphBuilder<Parameter<Self>>, dest: &mut Parameters) {
+    fn insert(item: Param<Self>, dest: &mut Parameters) {
         dest.oned_params.push(item);
     }
 }
 
 impl ParamDim for Ix2 {
-    fn insert(item: GraphBuilder<Parameter<Self>>, dest: &mut Parameters) {
+    fn insert(item: Param<Self>, dest: &mut Parameters) {
         dest.twod_params.push(item);
     }
 }
 
 impl ParamDim for Ix3 {
-    fn insert(item: GraphBuilder<Parameter<Self>>, dest: &mut Parameters) {
+    fn insert(item: Param<Self>, dest: &mut Parameters) {
         dest.threed_params.push(item);
     }
 }
 
 impl ParamDim for Ix4 {
-    fn insert(item: GraphBuilder<Parameter<Self>>, dest: &mut Parameters) {
+    fn insert(item: Param<Self>, dest: &mut Parameters) {
         dest.fourd_params.push(item);
     }
 }
 
 impl ParamDim for Ix5 {
-    fn insert(item: GraphBuilder<Parameter<Self>>, dest: &mut Parameters) {
+    fn insert(item: Param<Self>, dest: &mut Parameters) {
         dest.fived_params.push(item);
     }
 }
 
 impl ParamDim for Ix6 {
-    fn insert(item: GraphBuilder<Parameter<Self>>, dest: &mut Parameters) {
+    fn insert(item: Param<Self>, dest: &mut Parameters) {
         dest.sixd_params.push(item);
     }
 }
 
 impl ParamDim for IxDyn {
-    fn insert(item: GraphBuilder<Parameter<Self>>, dest: &mut Parameters) {
+    fn insert(item: Param<Self>, dest: &mut Parameters) {
         dest.dynd_params.push(item);
     }
 }
 
-#[derive(Debug, Clone)]
+struct Param<D>
+where
+    D: Dimension,
+{
+    id: usize,
+    input: Rc<Input<D>>,
+    input_diff: Rc<InputDiff<D>>,
+}
+
+impl<D> Clone for Param<D>
+where
+    D: Dimension,
+{
+    fn clone(&self) -> Self {
+        Self {
+            id: self.id,
+            input: self.input.clone(),
+            input_diff: self.input_diff.clone(),
+        }
+    }
+}
+
+#[derive(Clone)]
 /// Contains the learnable ancestors of the node.
 pub struct Parameters {
     // Contains the one dimensional learnable ancestors
-    oned_params: Vec<GraphBuilder<Parameter<Ix1>>>,
+    oned_params: Vec<Param<Ix1>>,
     // Contains the two dimensional learnable ancestors
-    twod_params: Vec<GraphBuilder<Parameter<Ix2>>>,
+    twod_params: Vec<Param<Ix2>>,
     // Contains the three dimensional learnable ancestors
-    threed_params: Vec<GraphBuilder<Parameter<Ix3>>>,
+    threed_params: Vec<Param<Ix3>>,
     // Contains the four dimensional learnable ancestors
-    fourd_params: Vec<GraphBuilder<Parameter<Ix4>>>,
+    fourd_params: Vec<Param<Ix4>>,
     // Contains the five dimensional learnable ancestors
-    fived_params: Vec<GraphBuilder<Parameter<Ix5>>>,
+    fived_params: Vec<Param<Ix5>>,
     // Contains the six dimensional learnable ancestors
-    sixd_params: Vec<GraphBuilder<Parameter<Ix6>>>,
+    sixd_params: Vec<Param<Ix6>>,
     // Contains the dynamic dimensional learnable ancestors
-    dynd_params: Vec<GraphBuilder<Parameter<IxDyn>>>,
+    dynd_params: Vec<Param<IxDyn>>,
 }
 
 impl Parameters {
@@ -108,41 +148,108 @@ impl Parameters {
     }
 }
 
-// A pointer to a node in the computational graph.
-#[derive(Debug)]
-pub struct GraphBuilder<T: Node> {
-    repr: Rc<T>,
-    grad: Option<RefCell<Tensor<T::Dim>>>,
-    upstream: Parameters,
+pub struct Var<T>
+where
+    T: Data + Forward + 'static,
+{
+    last: (usize, Rc<T>),
+    forward_path: BTreeMap<usize, Rc<dyn Forward>>,
 }
 
-impl<T: Node> Clone for GraphBuilder<T> {
-    fn clone(&self) -> Self {
-        GraphBuilder {
-            repr: Rc::clone(&self.repr),
-            grad: None,
-            upstream: self.upstream.clone(),
+impl<T> Var<T>
+where
+    T: Data + Forward + 'static,
+{
+    pub fn new(node: T) -> Self {
+        let (id, forward) = (unsafe { OPERATIONS_COUNTER.next() }, Rc::new(node));
+        let mut forward_path = BTreeMap::new();
+        forward_path.insert(id, forward.clone() as Rc<dyn Forward>);
+
+        Self {
+            last: (id, forward),
+            forward_path,
         }
     }
 }
 
-impl<D> GraphBuilder<Parameter<D>>
+impl<T> Clone for Var<T>
+where
+    T: Data + Forward + 'static,
+{
+    fn clone(&self) -> Self {
+        Self {
+            last: (self.last.0, self.last.1.clone()),
+            forward_path: self.forward_path.clone(),
+        }
+    }
+}
+
+impl<D> Var<Input<D>>
+where
+    D: ParamDim,
+{
+    pub fn requires_grad(self) -> VarDiff<Input<D>> {
+        let (id, forward) = self.last;
+        let backward = Rc::new(forward.differentiable());
+        let forward_path = self.forward_path;
+        let mut backward_path = BTreeMap::new();
+        backward_path.insert(id, backward.clone() as Rc<dyn Backward>);
+        let mut parameters = Parameters::new();
+        D::insert(
+            Param {
+                id,
+                input: forward.clone(),
+                input_diff: backward.clone(),
+            },
+            &mut parameters,
+        );
+        debug_assert_eq!(self.forward_path.contains_key(&id), true);
+        VarDiff {
+            last: (id, forward, backward),
+            forward_path,
+            backward_path,
+            parameters,
+        }
+    }
+}
+
+pub struct VarDiff<T>
+where
+    T: Data + Forward + Differentiable + 'static,
+{
+    last: (usize, Rc<T>, Rc<<T as Differentiable>::Output>),
+    forward_path: BTreeMap<usize, Rc<dyn Forward>>,
+    backward_path: BTreeMap<usize, Rc<dyn Backward>>,
+    parameters: Parameters,
+}
+
+impl<T> Clone for VarDiff<T>
+where
+    T: Data + Forward + Differentiable + 'static,
+{
+    fn clone(&self) -> Self {
+        Self {
+            last: (self.last.0, self.last.1.clone(), self.last.2.clone()),
+            forward_path: self.forward_path.clone(),
+            backward_path: self.backward_path.clone(),
+            parameters: self.parameters.clone(),
+        }
+    }
+}
+
+impl<D> VarDiff<Input<D>>
 where
     D: ParamDim,
 {
     pub fn grad(&self) -> Ref<Tensor<D>> {
-        self.repr.deref().grad.borrow()
+        self.last.2.gradient()
     }
 
     pub(crate) fn data_mut(&mut self) -> RefMut<Tensor<D>> {
-        self.repr.data.borrow_mut()
-    }
-
-    fn as_ptr(&self) -> *const Parameter<D> {
-        self.repr.deref() as *const Parameter<D>
+        self.last.1.data_mut()
     }
     fn zero_grad(&mut self) {
-        self.repr.zero_grad()
+        self.last.2.zero_grad()
     }
 }
 
@@ -208,15 +315,15 @@ where
 //     }
 // }
 
-impl<T: Node> GraphBuilder<T> {
-    pub(super) fn new(repr: Rc<T>, upstream: Parameters) -> Self {
-        GraphBuilder {
-            repr,
-            grad: None,
-            upstream,
-        }
-    }
-}
+// impl<T: Node> GraphBuilder<T> {
+//     pub(super) fn new(repr: Rc<T>, upstream: Parameters) -> Self {
+//         GraphBuilder {
+//             repr,
+//             grad: None,
+//             upstream,
+//         }
+//     }
+// }
 
 //     pub fn backward(&mut self, seed: f32) {
 //         let data_ref: &Tensor<T::Dim> = &self.repr.data();
--- src/graph/node/forward.rs
+++
@@ -1,3 +0,0 @@
-pub trait Forward {
-    fn forward(&mut self);
-}
